c6fb40b187ddfa4e5b253db5b29ec5bdc3238a0a
Tarik <tarik_can@live.nl>
2017-09-14 10:39:09 +0200

    les 3 opdracht 1 synchronizatie

---
+++ Les3/pe3_1.py
@@ -0,0 +1,11 @@
+score = int(input("Wat is uw score? : "))
+
+if score >= 15:
+    print("Gefeliciflapstaart, u bent geslaagd!")
+    print("Met een score van: " + str(score) + " heeft u geslaagd!")
+
+else:
+    print("Jammer, u bent gezakt!")
+    print("Met een score van: " + str(score) + " heeft u gezakt!")
+
+print("Nog een fijne dag!")
