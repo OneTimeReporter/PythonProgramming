4eddfb11b72742d265942424bde161dbd04573fc
Tarik <tarik_can@live.nl>
2017-09-14 11:23:51 +0200

    les 3 opdracht 1 synchronizatie

---
+++ Les3/pe3_3.py
@@ -0,0 +1,9 @@
+paspoortcheck = input("Heeft U een paspoort? True of False: ")
+leeftijd = int(input("Wat is uw leeftijd?: "))
+
+
+if paspoortcheck == 'True' and leeftijd >= 18:
+    print("U mag stemmen!")
+
+else:
+    print("U mag nog niet stemmen")
---
+++ Les3/pe3_6.py
@@ -0,0 +1,6 @@
+s="Guido van Rossum heeft programmeertaal Python bedacht."
+
+
+for char in s:
+    if char in 'aeiou':
+        print(char)
